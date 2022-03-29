44b94205e2a9ee1539a8b23e6ce1539e38c3fd5f
D-Dawnsin <dawson.dawnsin@gmail.com>
2022-03-29 16:50:11 -0500

    changes to match my tables

--- connect.php
+++ connect.php
@@ -2,13 +2,13 @@
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 
-$con = mysqli_connect('localhost', 'root', '','emails');
+$con = mysqli_connect('localhost', 'root', '','test');
 
 // get the post records
 $email = $_POST['email'];
 
 // database insert SQL code
-$sql = "INSERT INTO `test` (`email`) VALUES ('$email')";
+$sql = "INSERT INTO `emails` (`email`) VALUES ('$email')";
 
 // insert in database 
 $rs = mysqli_query($con, $sql);
